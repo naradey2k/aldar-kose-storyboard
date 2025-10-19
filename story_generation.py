12805d4a4614d82a15c7b3d2c78b07c4826a5826
doksterium <56970175+naradey2k@users.noreply.github.com>
2025-10-19 12:43:07 +0500

    Update story_generation.py

--- story_generation.py
+++ story_generation.py
@@ -199,19 +199,20 @@ def generate_images_from_json(client, json_path="index.json"):
         prompt = scene.get("prompt", "")
         neg = scene.get("neg", "low quality, inconsistent, cartoon, blur")  
 
-        if sid == 1:
+        if sid:
+        # if sid == 1:
             text_input = """Use the provided image of a Aldar Kose as reference image how my character must and only look like. Generate """ + prompt
             response = client.models.generate_content(
                 model="gemini-2.5-flash-image",
                 contents=[text_input, ref_image],
-            )
-        else:
-            ctx = Image.open(f"results/frame_{sid -1:02}.png")
-            text_input = f"""Use the provided image as a context of what happened in previous scene. Generate """ + prompt
-            response = client.models.generate_content(
-                model="gemini-2.5-flash-image",
-                contents=[text_input, ctx],
-            )
+        #     )
+        # else:
+        #     ctx = Image.open(f"results/frame_{sid -1:02}.png")
+        #     text_input = f"""Use the provided image as a context of what happened in previous scene. Generate """ + prompt
+        #     response = client.models.generate_content(
+        #         model="gemini-2.5-flash-image",
+        #         contents=[text_input, ctx],
+        #     )
 
 
         image_parts = [
